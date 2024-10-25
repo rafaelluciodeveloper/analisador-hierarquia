81448afd6d1d82afea3b2f11e84e7ce5f250dbfd
Rafael Lucio <rafaellucio.developer@gmail.com>
2024-10-25 18:01:07 -0300

    feat(cli): add function to load hierarchy JSON

---
+++ cli/src/utils/loadHierarchy.ts
@@ -0,0 +1,8 @@
+import * as fs from 'fs';
+import path from 'path';
+
+export function loadHierarchy() {
+  const filePath = path.resolve(__dirname, '../../dicts/hierarchy.json');
+  const data = fs.readFileSync(filePath, 'utf-8');
+  return JSON.parse(data);
+}
